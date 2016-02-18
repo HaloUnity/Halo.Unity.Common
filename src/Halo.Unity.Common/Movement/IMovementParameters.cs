6e75f059131de53f30453432e29d2c3f2a7248c6
Andrew Blakely <andrew.blakely@ymail.com>
2016-02-18 00:10:53 -0600

    Added PlayerMotor and dependent services

---
+++ src/Halo.Unity.Common/Movement/IMovementParameters.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Halo.Unity.Common
+{
+	/// <summary>
+	/// Provides movement parameters.
+	/// Allows for abstraction of input source (Network/Local/Rewind)
+	/// </summary>
+	public interface IMovementParameters
+	{
+		/// <summary>
+		/// Desired Jump intensity.
+		/// </summary>
+		float JumpIntensity { get; }
+
+		/// <summary>
+		/// Desired movement speed.
+		/// </summary>
+		float MovementSpeed { get; }
+    }
+}
---
+++ src/Halo.Unity.Common/Movement/ITimeSimulation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Halo.Unity.Common
+{
+	/// <summary>
+	/// Implementer is a simulation object that can be stepped forward in time.
+	/// </summary>
+	public interface ITimeSimulation
+	{
+		/// <summary>
+		/// Simulate forward by the desired delta.
+		/// </summary>
+		/// <param name="dt">Time delta.</param>
+        void Simulate(float dt);
+	}
+}
